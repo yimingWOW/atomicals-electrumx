5dafa6f821b57c86ecbc550f907f42e8f217be81
archx <archx@atomicals.xyz>
2023-09-25 21:39:20 -0500

    wip

--- electrumx/server/session.py
+++ electrumx/server/session.py
@@ -1267,10 +1267,9 @@ class ElectrumX(SessionBase):
 
     async def get_summary_info(self, atomical_hash_count=10):
 
-        if atomical_hash_count and atomical_hash_count > 2000:
-            atomical_hash_count = 2000
-        else:
-            atomical_hash_count = 10
+        if not atomical_hash_count:
+            if atomical_hash_count > 2000:
+                atomical_hash_count = 2000
 
         db_height = self.db.db_height
         last_block_hash = self.db.get_atomicals_block_hash(db_height)
