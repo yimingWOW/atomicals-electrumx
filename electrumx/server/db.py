89103867e39c5a62551764f1c1314c70d3cf6625
archx <archx@atomicals.xyz>
2023-10-03 13:14:35 -0500

    load mods

--- electrumx/server/db.py
+++ electrumx/server/db.py
@@ -1464,7 +1464,7 @@ class DB:
         for item in arr:
             modfile.write(item + '\n')
         for modobj in modobjs:
-            modfile.write(loads(modobj) + '\n')
+            modfile.write(str(loads(modobj)) + '\n')
         
         modfile.close() 
 
