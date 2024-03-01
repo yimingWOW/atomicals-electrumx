5d9d35142983591f8c3ba6279e7e7676f96fe12f
yiming <isyiming.zhang@icloud.com>
2024-03-01 21:42:29 +0800

    add log

--- electrumx/lib/atomicals_blueprint_builder.py
+++ electrumx/lib/atomicals_blueprint_builder.py
@@ -390,10 +390,10 @@ class AtomicalsTransferBlueprintBuilder:
             output_colored_map[expected_output_index] = output_colored_map.get(expected_output_index) or {'atomicals': {}}
             output_colored_map[expected_output_index]['atomicals'][atomical_id] = AtomicalColoredOutputFt(atomical_info.exponent, txout.value, get_nominal_token_value(txout.value, atomical_info.exponent), atomical_info)
             
-            if isinstance(atomical_info, ExpectedOutputSet):
-                print(f"Unexpected type for atomical_info: {type(atomical_info)}")
-            else:
-                print(f"Unexpected type for atomical_info: {type(atomical_info)}")
+            # if isinstance(atomical_info, ExpectedOutputSet):
+            #     print(f"Unexpected type for atomical_info: {type(atomical_info)}")
+            # else:
+            #     print(f"Unexpected type for atomical_info: {type(atomical_info)}")
       return AtomicalFtOutputBlueprintAssignmentSummary(output_colored_map, ft_coloring_summary.fts_burned, ft_coloring_summary.cleanly_assigned, first_atomical_id) 
   
   @classmethod
