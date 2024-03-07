5e1e4d2d406f99bdd83728f8e75e7c841380be5a
yiming <isyiming.zhang@icloud.com>
2024-03-07 12:19:51 +0800

    add log

--- electrumx/server/block_processor.py
+++ electrumx/server/block_processor.py
@@ -497,7 +497,7 @@ class BlockProcessor:
         genesis_activation = self.coin.GENESIS_ACTIVATION
 
         for block in blocks:
-            print(f'mint, block.height= {block.height}')
+            print(f'mint, block= {block}')
             height += 1
             is_unspendable = (is_unspendable_genesis if height >= genesis_activation
                               else is_unspendable_legacy)
